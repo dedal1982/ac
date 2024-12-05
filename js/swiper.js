6345a23e5490c521365f11839c0c6b27e1df46fe
aldm <d44.a1@yandex.ru>
2024-12-05 04:26:19 +0300

    1

--- js/swiper.js
+++ js/swiper.js
@@ -41,3 +41,27 @@ const accountSwiper = new Swiper(".account__swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+const swiperPre = new Swiper(".in-demand__grid-pre", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 26,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
