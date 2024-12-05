2d95d34901d9848b9f9ae21d41ec9203341305ca
aldm <d44.a1@yandex.ru>
2024-12-05 04:50:58 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const moreButton = document.querySelector(".in-demand__more");
 const preItems = document.querySelector(".in-demand__grid-pre");
 const mainItems = document.querySelector(".in-demand__swiper");
+const backButton = document.querySelector(".in-demand__back");
 
 if (moreButton) {
   moreButton.addEventListener("click", () => {
     preItems.classList.add("active");
     mainItems.classList.add("active");
     moreButton.classList.add("active");
+    backButton.classList.add("active");
   });
 }
